9fc5ae010ce540eaae44dd3519e0a995ed9fa3a8
kellyoliveira473 <kellyoliveira473@gmail.com>
2025-08-21 20:01:55 -0300

    Atualizando..

--- src/main/java/com/kelly/Compra_usuario_padaria/WebConfig.java
+++ src/main/java/com/kelly/Compra_usuario_padaria/WebConfig.java
@@ -1,5 +1,6 @@
 
 package com.kelly.Compra_usuario_padaria;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
